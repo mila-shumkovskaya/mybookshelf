10b4cd82932e287922aeaedb3cf9c82476f99f3f
mila_shumkovskaya <mila_shumkovskaya@mail.ru>
2020-12-20 16:38:43 +0300

    get Book to DetailsActivity and it's fragments

--- app/src/main/java/com/study/mybookshelf/DetailsActivity.kt
+++ app/src/main/java/com/study/mybookshelf/DetailsActivity.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.study.mybookshelf.model.Book
+import com.study.mybookshelf.model.BorrowedBook
+import com.study.mybookshelf.model.LendedBook
+import com.study.mybookshelf.ui.BorrowedBookDetailsFragment
+import com.study.mybookshelf.ui.LendedBookDetailsFragment
 import com.study.mybookshelf.ui.LibraryBookDetailsFragment
 
 
@@ -16,10 +21,29 @@ class DetailsActivity : AppCompatActivity() {
         val fragmentTransaction: FragmentTransaction = fragmentManager
                 .beginTransaction()
 
+        val book= intent.getSerializableExtra("book")
 
         // добавляем фрагмент
-        val myFragment = LibraryBookDetailsFragment()
-        fragmentTransaction.add(R.id.container, myFragment)
-        fragmentTransaction.commit()
+
+        if(book is LendedBook)
+        {
+            val myFragment = LendedBookDetailsFragment()
+            fragmentTransaction.add(R.id.container, myFragment)
+            fragmentTransaction.commit()
+        }else
+        if(book is BorrowedBook)
+        {
+            val myFragment = BorrowedBookDetailsFragment()
+            fragmentTransaction.add(R.id.container, myFragment)
+            fragmentTransaction.commit()
+        }
+        else
+        if(book is Book)
+        {
+            val myFragment = LibraryBookDetailsFragment()
+            fragmentTransaction.add(R.id.container, myFragment)
+            fragmentTransaction.commit()
+        }
+
     }
 }
--- app/src/main/java/com/study/mybookshelf/ui/BorrowedBookDetailsFragment.kt
+++ app/src/main/java/com/study/mybookshelf/ui/BorrowedBookDetailsFragment.kt
@@ -14,25 +14,25 @@ import com.study.mybookshelf.model.Book
 import com.study.mybookshelf.ui.BookRecyclerView.BooksRecyclerView
 import kotlinx.android.synthetic.main.fragment_library_book_details.*
 
-class LibraryBookDetailsFragment: Fragment() {
+class BorrowedBookDetailsFragment: Fragment() {
 
-   // lateinit var libraryViewModel:LibraryViewModel
+    // lateinit var libraryViewModel:LibraryViewModel
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-     //   libraryViewModel = ViewModelProviders.of(this).get(LibraryViewModel::class.java)
-       val root = inflater.inflate(R.layout.fragment_library_book_details, container, false)
-       val book:Book = requireActivity().intent.getSerializableExtra("book") as Book
-       val title: EditText = root.findViewById(R.id.et_title)
-       val author: EditText = root.findViewById(R.id.et_author)
-       title.hint=book.title
-       author.hint=book.author
-       // val rvBooks: BooksRecyclerView =  root.findViewById(R.id.recycler_view_books)
+        //   libraryViewModel = ViewModelProviders.of(this).get(LibraryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_library_book_details, container, false)
+        val book:Book = requireActivity().intent.getSerializableExtra("book") as Book
+        val title: EditText = root.findViewById(R.id.et_title)
+        val author: EditText = root.findViewById(R.id.et_author)
+        title.hint=book.title
+        author.hint=book.author
+        // val rvBooks: BooksRecyclerView =  root.findViewById(R.id.recycler_view_books)
         //libraryViewModel.libraryBooksList.observe(viewLifecycleOwner, Observer {
         //    rvBooks.adapter.refreshBooks(it)
-       // })
+        // })
 
         return root
     }
